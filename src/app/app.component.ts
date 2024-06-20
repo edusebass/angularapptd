c98b95d4fcad7df76da419589dadb716169d2281
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-06-19 22:03:15 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
+})
+export class AppComponent {
+  title = 'todoappangular';
+  palabra = 'esta viene directaemte del app.componet .ts';
+  tasks = [
+    'Learn Angular',
+    'Learn React',
+    'Learn Vue',
+    'Learn Angular',
+    'Learn React',
+    'Learn Vue'
+  ]
+}
