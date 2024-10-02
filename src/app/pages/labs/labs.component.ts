8ba4b911a42861116c1f1224304e0bde777492b0
edusebass <edu03sebas@gmail.com>
2024-10-01 21:22:43 -0500

    Directivas de control

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -9,14 +9,14 @@ import { CommonModule } from '@angular/common';
   styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-  tasks = [
+  tasks = signal([
     'Learn Angular',
     'Learn React',
     'Learn Vue',
     'Learn Angular',
     'Learn React',
     'Learn Vue',
-  ];
+  ]);
   // name = 'Eduardo';
   img =
     'https://bs-uploads.toptal.io/blackfish-uploads/components/seo/5911499/og_image/optimized/top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png';
